87cdeeb2296c8c927269c9e90a3198e90dac9f14
kuromamekuromame88 <tei8sia@outlook.jp>
2026-01-01 14:22:35 +0900

    あ

--- SimpleToDo/SimpleToDo/Preview Content/ToDoItem+Preview.swift
+++ SimpleToDo/SimpleToDo/Preview Content/ToDoItem+Preview.swift
@@ -10,5 +10,6 @@ import Foundation
 /// Code in this folder is not included when creating an archive to send to the app store, in the same way that the code for SwifUI Previews are stripped out of archived builds
 extension ToDoItem {
     /// An example property that's used for Xcode previewing.
-    static let example = ToDoItem()
+    static let example = ExampleToDoItem()
+    //ToDoItem()から変更
 }
